a01deb7923243a17dff726fef2570ea6c7d78ec1
Michel Goñi <michel@meep.me>
2022-01-20 14:11:21 +0100

    Added initial API response

---
+++ LightPrice WatchKit Extension/ElectricityPriceApi/LightPriceResponse.swift
@@ -0,0 +1,44 @@
+//
+//  LightPriceResponse.swift
+//  LightPrice WatchKit Extension
+//
+//  Created by Miguel Goñi on 20/1/22.
+//
+
+import Foundation
+
+public struct LightPriceResponse: Codable, Equatable {
+    public static func == (lhs: LightPriceResponse, rhs: LightPriceResponse) -> Bool {
+        return lhs.indicator.name == rhs.indicator.name
+    }
+    
+    let indicator: Indicator
+}
+
+// MARK: - Indicator
+public struct Indicator: Codable {
+    let name: String
+    let valuesUpdatedAt: String
+    let values: [Value]
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case valuesUpdatedAt = "values_updated_at"
+        case values
+    }
+}
+
+
+// MARK: - Value
+public struct Value: Codable {
+    let value: Double
+    let datetime: String
+    let geoID: Int
+    let geoName: String
+
+    enum CodingKeys: String, CodingKey {
+        case value, datetime
+        case geoID = "geo_id"
+        case geoName = "geo_name"
+    }
+}
